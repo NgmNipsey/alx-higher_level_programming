eb1059667a0af8f9a517062e797bdd16427c696f
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-11-29 13:59:59 -0800

    Match bytecode

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Match bytecode provided."""
+
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
