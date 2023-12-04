0682b1cd1a057fc8b0af95e79cf06fecac33e692
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-12-04 10:38:10 -0800

    Retrieves an elements from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A function that retrieves an elements from a list."""
+
+
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list) - 1:
+        return (None)
+    else:
+        return (my_list[idx])
