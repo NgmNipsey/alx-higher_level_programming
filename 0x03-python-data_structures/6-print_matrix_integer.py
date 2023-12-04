afe4cbb31b3820ec2d7bf9e2bd35a51212c73346
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-12-04 12:01:33 -0800

    Lists of lists

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Print a Matrix of integers."""
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
