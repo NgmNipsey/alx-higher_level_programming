445f9cf97c70feb3c269f6f3446a9fe5fc1a7431
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2023-12-18 06:36:22 -0800

    Exception handling

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A function that prints x elements of a list."""
+
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return (count)
