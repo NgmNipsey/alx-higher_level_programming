e3293744a311e2b4011c50521bbed46249c29562
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2024-01-29 17:24:01 -0800

    Definition of a rectangle with area, perimeter ....

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         else:
-            return "\n".join([str(print_symbol) * self.width] * self.height)
+            return "\n".join([str(self.print_symbol) * self.width] * self.height)
 
     def __repr__(self):
         """ Returns string representation of an instance. """
