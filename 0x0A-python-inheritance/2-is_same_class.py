47797ded8a065bf26d7681c0b0ab135d1fa8a8c2
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2024-02-05 16:04:11 -0800

    Returns true otherwise false

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" function that returns true otherwise false """
+
+
+def is_same_class(obj, a_class):
+    """ Represent a function """
+    if type(obj) == a_class:
+        return True
+    return False
